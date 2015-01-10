772a1eb11f06a8e797713133396a33f2257be9e3
mtraina <matteo.traina.mail@gmail.com>
2015-01-10 14:56:38 +0000

    google optionals

--- dropwizard-rest/src/main/java/com/mtraina/rest/DropwizardRestResource.java
+++ dropwizard-rest/src/main/java/com/mtraina/rest/DropwizardRestResource.java
@@ -1,13 +1,13 @@
 package com.mtraina.rest;
 
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Path("/")
@@ -26,7 +26,7 @@ public class DropwizardRestResource {
     @GET
     @Timed
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
-        final String value = String.format(template, name.orElse(defaultName));
+        final String value = String.format(template, name.or(defaultName));
         return new Saying(counter.incrementAndGet(), value);
     }
 }
